320f772543ed200cd54da164275d49b037415c38
Leonardo De Nigro <kninox@MacBook-Pro-di-Leonardo.local>
2021-11-01 23:44:07 +0100

    Double quote

--- get-quote.py
+++ get-quote.py
@@ -11,6 +11,30 @@ def primary():
 
 
   print(quotes[rnd]) 
-
+  
 if __name__== "__main__":
+  
   primary()
+  
+import random
+last=13
+rnd=random.randint(0,last)
+
+def secondary():
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
+
+  print(quotes[rnd])
+
+  
+if __name__== "__main__":
+  secondary()
+
+
+
+
+
+
+
+  
